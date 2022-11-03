c12ad48893608ecf795fdc621471d5b8c2857286
avishkahiran <avishkahiranb@gmail.com>
2022-11-04 04:19:54 +0530

    Navigation setup

--- App.js
+++ App.js
@@ -1,43 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import logo from './assets/logo.png'
-import { Button } from 'react-native-paper';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+import LoadScreen from './components/LoadScree';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logoStyle}
-        source={logo}
-      />
-      <Text style={styles.txtStyle}>Tea Collector</Text>
-
-      <Button icon="coffee-to-go" style={styles.startBtn} mode="contained" buttonColor="#000" onPress={() => console.log('Pressed')}>
-        Let's Start
-      </Button>
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer initialRouteName="LoadScreen">
+      <Stack.Navigator>
+        <Stack.Screen name="LoadScreen" options={{headerShown: false}} component={LoadScreen} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09B44D',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  txtStyle: {
-    color:'#fff',
-    fontSize:25,
-    fontWeight:"bold",
-  },
-  logoStyle: {
-    width: 100,
-    height: 125,
-  },
-  startBtn: {
-    marginTop:20
-  },
-});
